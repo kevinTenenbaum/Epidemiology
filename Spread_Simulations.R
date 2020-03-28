2b601bb40f2ec3baa226614177dede9ca1e209d3
Kevin Tenenbaum <ktenenbaum@rstudioserver.us-central1-a.c.stable-century-259004.internal>
2020-03-28 04:53:30 +0000

    Added the ability to model multiple cities

--- Spread_Simulations.R
+++ Spread_Simulations.R
@@ -12,14 +12,19 @@ InfectionRadius <- 2
 InfectionRate <- .2
 InfectionDays <- 7
 InfectionDaySD <- 1
-N <- 1000
+N <- 10000
 SideLength <- 50
 MoveSD = 3
 NumDays <- 100 
+NCities <- 4
+CityMoveRate <- 0.1
 DayStats <- data.frame(Day = 1:NumDays, 
                        Susceptible = rep(0, NumDays),
                        Infected = rep(0, NumDays),
                        Removed = rep(0, NumDays))
+CityStats <- bind_rows(lapply(1:NCities, function(x) DayStats)) %>% arrange(Day)
+CityStats$City <- 1:NCities
+CityStats <- CityStats[, c('Day','City','Susceptible','Infected','Removed')]
 
 
 
@@ -28,7 +33,8 @@ people <- tibble(ID = 1:N,
        Status = ifelse(InitialInfected == 1, 'I', 'S'),
        StartingLocationX = runif(N, min = -SideLength/2, max = SideLength/2), 
        StartingLocationY = runif(N, min = -SideLength/2, max = SideLength/2),
-       NDays = 0
+       NDays = 0,
+       City = sample(1:NCities, size = N, replace = TRUE)
        )
 people <- data.table(people)
 setkey(people, ID)
@@ -40,18 +46,29 @@ getSummaryStats <- function(people){
   
 }
 
-newSim <- list(people = people, summary = getSummaryStats(people))
-DayStats[1, -1] <- getSummaryStats(people)
+getCityStats <- function(people){
+  cs <- people[,.(Susceptible = sum(Status == 'S'),
+            Infected = sum(Status == 'I'),
+            Removed = sum(Status == 'R')), .(City)]
+  setorder(cs, City)
+  return(cs)
+}
+
+newSim <- list(people = people, summary = getSummaryStats(people), cities = getCityStats(people))
+DayStats[1, -1] <- newSim$summary
+CityStats[CityStats$Day == 1, -1] <- newSim$cities
+
 pb <- txtProgressBar(min = 2, max = NumDays, style = 3)
 for(i in 2:NumDays){
   setTxtProgressBar(pb, i)
   newSim <- simDay(newSim$people)
   DayStats[i, -1] <- newSim$summary
+  CityStats[CityStats$Day == i, -1] <- newSim$cities
 }
 
-ggplot() + geom_line(data = DayStats, aes(x = Day, y = Infected), color = 'red') + 
-  geom_line(data = DayStats, aes(x = Day, y = Susceptible), color  = 'black') + 
-  geom_line(data = DayStats, aes(x = Day, y = Removed), color = 'green') 
+ggplot() + geom_line(data = CityStats, aes(x = Day, y = Infected), color = 'red') + 
+  geom_line(data = CityStats, aes(x = Day, y = Susceptible), color  = 'black') + 
+  geom_line(data = CityStats, aes(x = Day, y = Removed), color = 'green') + facet_wrap(~City)
 
 
 simDay <- function(people){
@@ -65,14 +82,14 @@ simDay <- function(people){
      ID = people[Status == 'S',ID]) # Created cross-joined data frame
   
   setkey(merged, Infected) # Merge infected people data in
-  merged = merged[infected[,list(ID, StartingLocationX, StartingLocationY)], nomatch = 0]
+  merged = merged[infected[,list(ID, City, StartingLocationX, StartingLocationY)], nomatch = 0]
   
   setkey(merged, ID) # Merge susceptible people data in
-  merged = merged[people[,list(ID, StartingLocationX, StartingLocationY)], nomatch = 0]
+  merged = merged[people[,list(ID, City, StartingLocationX, StartingLocationY)], nomatch = 0]
   
   # Check if they were in radius
   merged[, Distance := sqrt((StartingLocationX - i.StartingLocationX)^2 + (StartingLocationY + i.StartingLocationY)^2)]
-  merged[, inRadius := Distance <= InfectionRadius]
+  merged[, inRadius := Distance <= InfectionRadius & City == i.City]
   
   # Aggregate to only people that were in radius
   aggMerged <- merged[,.(NumContacts = sum(inRadius)), .(ID)]
@@ -80,7 +97,7 @@ simDay <- function(people){
   
   # Randomly sample exposed people to be infected
   toInfect[, Infected := sample(c(0,1), size = nrow(toInfect), replace = TRUE, prob = c(1-InfectionRate, InfectionRate))]
-  
+  setkey(toInfect, ID)
   ## Change Status 
   people <- toInfect[people] # Join infection data
   people[,NewStatus := Status] # Set new status as the same as current status
@@ -94,11 +111,16 @@ simDay <- function(people){
   people[,Status := NewStatus] # Finalize status changes
     
   # Move Everyone to new locations
-  people[, c("StartingLocationX", "StartingLocationY") := list(rnorm(1000, mean = StartingLocationX, sd = MoveSD), rnorm(1000, mean = StartingLocationY, sd = MoveSD))]
-  people <- people[, c("ID", "Status", "StartingLocationX", "StartingLocationY", "NDays")]
-  
+  people[, c("StartingLocationX", "StartingLocationY") := list(rnorm(N, mean = StartingLocationX, sd = MoveSD), rnorm(N, mean = StartingLocationY, sd = MoveSD))]
+  people <- people[, c("ID", "City", "Status", "StartingLocationX", "StartingLocationY", "NDays")]
+  people[, MoveCity := runif(nrow(people)) <= CityMoveRate]
+  people[, CityIndex := sample(1:(NCities-1), size = nrow(people), replace = TRUE)]
+  people[,NewCity := City]
+  people[MoveCity == TRUE, NewCity := ifelse(CityIndex < City, CityIndex, CityIndex + 1)]
+  people[,City := NewCity]
   # Calculate summary stats
   sumStats <- getSummaryStats(people)
+  cityStats <- getCityStats(people)
   
-  return(list(people = people, summary = sumStats))
+  return(list(people = people, summary = sumStats, cities = cityStats))
 }
